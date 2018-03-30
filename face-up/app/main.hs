a8bab174452754e2b85012fc53a91f83880df3bd
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-30 11:21:02 -0400

    display first frame

--- face-up/app/main.hs
+++ face-up/app/main.hs
@@ -1,7 +1,39 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase, OverloadedStrings, TypeApplications #-}
+import Codec.FFmpeg
+import Codec.Picture
+import Control.Exception
+import Graphics.Gloss
+import Graphics.Gloss.Juicy
 import Options.Generic
+
+data Video = Video
+  { videoNextFrame :: IO (Maybe (Image PixelRGB8))
+  , videoCleanup   :: IO ()
+  }
+
+withVideo :: FilePath
+          -> (Video -> IO a)
+          -> IO a
+withVideo filePath = bracket acquire release
+  where
+    acquire :: IO Video
+    acquire = fmap (uncurry Video)
+            . imageReader
+            . File
+            $ filePath
+
+    release :: Video -> IO ()
+    release = videoCleanup
 
 main :: IO ()
 main = do
+  initFFmpeg
   filePath <- getRecord "face-up"
-  putStrLn $ filePath ++ ": unsupported file format"
+  withVideo filePath $ \video -> do
+    videoNextFrame video >>= \case
+      Nothing -> putStrLn "empty video."
+      Just image -> do
+        let picture = fromImageRGB8 image
+        display (InWindow "Nice Window" (imageWidth image, imageHeight image) (10, 10))
+                white
+                picture
