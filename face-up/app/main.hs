23a7eca50608096522f9daae79a95aeeb2212d30
Samuel Gélineau <gelisam+github@gmail.com>
2018-03-30 12:05:30 -0400

    FaceUp.Video

--- face-up/app/main.hs
+++ face-up/app/main.hs
@@ -1,50 +1,9 @@
-{-# LANGUAGE LambdaCase, OverloadedStrings, TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
 import Codec.FFmpeg
-import Codec.Picture
-import Control.Exception
-import Control.Lens
-import Data.Maybe
-import Graphics.Gloss.Interface.IO.Animate
-import Graphics.Gloss.Juicy
 import Options.Generic
 
-data Video = Video
-  { videoNextFrameTime :: IO (Maybe (Image PixelRGB8, Double))
-  , videoCleanup       :: IO ()
-  }
+import FaceUp.Video
 
-videoNextFrame :: Video -> IO (Maybe (Image PixelRGB8))
-videoNextFrame video = over _Just fst <$> videoNextFrameTime video
-
-withVideo :: FilePath
-          -> (Video -> IO a)
-          -> IO a
-withVideo filePath = bracket acquire release
-  where
-    acquire :: IO Video
-    acquire = fmap (uncurry Video)
-            . imageReaderTime
-            . File
-            $ filePath
-
-    release :: Video -> IO ()
-    release = videoCleanup
-
-
-defaultImage :: Image PixelRGB8
-defaultImage = generateImage (\_ _ -> PixelRGB8 0 0 0) 640 480
-
-playVideo :: String -> Video -> IO ()
-playVideo windowTitle video = do
-  firstFrame <- fromMaybe defaultImage <$> videoNextFrame video
-
-  let nextFrame :: Float -> IO Picture
-      nextFrame _ = maybe mempty fromImageRGB8 <$> videoNextFrame video
-
-  animateFixedIO (InWindow windowTitle (imageWidth firstFrame, imageHeight firstFrame) (10, 10))
-                 black
-                 nextFrame
-                 mempty
 
 main :: IO ()
 main = do
