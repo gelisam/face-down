4104c543ef4a125ef96de3caeeecf43af0df0dc3
Samuel Gélineau <gelisam+github@gmail.com>
2018-05-02 09:23:05 -0400

    don't cache the pictures

--- face-trace/src/FaceTrace/Frame.hs
+++ face-trace/src/FaceTrace/Frame.hs
@@ -13,8 +13,10 @@ frameWidth = imageWidth
 frameHeight :: Frame -> Int
 frameHeight = imageHeight
 
+-- don't cache the pictures, or the cache will quickly consume all the RAM
 framePicture :: Frame -> Picture
-framePicture = fromImageRGB8
+framePicture frame = let Bitmap width height imgData _cacheMe = fromImageRGB8 frame
+                      in Bitmap width height imgData False
 
 
 defaultFrame :: Frame
