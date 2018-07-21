0c4c1f18a2345bde993acc5315b8fd558639cf6c
Samuel Gélineau <gelisam+github@gmail.com>
2018-07-21 08:26:50 -0400

    withPicureStream

--- face-trace/src/FaceTrace/Stream.hs
+++ face-trace/src/FaceTrace/Stream.hs
@@ -3,8 +3,11 @@ module FaceTrace.Stream where
 
 import Codec.FFmpeg
 import Control.Lens
+import Data.Maybe
+import Graphics.Gloss
 
 import Control.Lens.SetterM
+import Control.Monad.Extra
 import FaceTrace.Frame
 import FaceTrace.ReloadableRef
 import FaceTrace.Types
@@ -34,8 +37,47 @@ nextFrame stream = nextFrameTime stream
                  & mapped . _Just %~ fst
 
 
+-- The raw, unscaled frames
 withFrameStream :: FilePath -> (Stream Frame -> IO a) -> IO a
 withFrameStream filePath body = do
   (pixelWidth, pixelHeight) <- videoDimentions filePath
   withReloadableRef' (imageReaderTime (File filePath)) $ \reloadableRef -> do
     body $ Stream pixelWidth pixelHeight reloadableRef
+
+-- Unlike 'withFrameStream', this function honors the pixel aspect ratio
+withPictureStream :: FilePath -> (Stream Picture -> IO a) -> IO a
+withPictureStream filePath body = do
+  pixelAspectRatio <- videoPixelAspectRatio filePath -- width:height
+                  <&> fromMaybe 1
+  withFrameStream filePath $ \frameStream -> do
+    let pixelWidth, pixelHeight :: Int
+        pixelWidth  = view streamWidth  frameStream
+        pixelHeight = view streamHeight frameStream
+
+    -- stretch the image up, not down
+    let displayWidth, displayHeight :: Int
+        (displayWidth, displayHeight) =
+          if pixelAspectRatio > 0
+          then (round (pixelAspectRatio * fromIntegral pixelWidth), pixelHeight)
+          else (pixelWidth, round (fromIntegral pixelHeight / pixelAspectRatio))
+
+    let scaleX, scaleY :: Float
+        scaleX = fromIntegral displayWidth / fromIntegral pixelWidth
+        scaleY = fromIntegral displayHeight / fromIntegral pixelHeight
+
+    frameStream & toPictureStream
+                & scalePictureStream scaleX scaleY
+                & body
+
+
+toPictureStream :: Stream Frame -> Stream Picture
+toPictureStream = over (unSetterM streamFrame) framePicture
+
+
+scalePictureStream :: Float -> Float -> Stream Picture -> Stream Picture
+scalePictureStream scaleX scaleY = streamWidth           %~ scaleInt scaleX
+                               >>> streamHeight          %~ scaleInt scaleY
+                               >>> unSetterM streamFrame %~ scale scaleX scaleY
+  where
+    scaleInt :: Float -> Int -> Int
+    scaleInt factor = round . (* factor) . fromIntegral
