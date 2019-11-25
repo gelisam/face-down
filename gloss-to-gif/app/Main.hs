8f7f34b49dc3dbbb53642c9cf12dba4bfd4434c4
Samuel Gélineau <gelisam+github@gmail.com>
2019-11-24 19:58:16 -0500

    support Active

--- gloss-to-gif/app/Main.hs
+++ gloss-to-gif/app/Main.hs
@@ -2,7 +2,6 @@
 module Main where
 
 import Data.Active
-import Data.Functor.Apply
 import Graphics.Gloss
 
 import GlossToGif
@@ -11,29 +10,29 @@ import GlossToGif
 main
   :: IO ()
 main = do
-  renderGif "out.gif" (300, 300) dynamic
+  renderGif "out.gif" (300, 300) active
 
 
 sine
   :: Floating a
-  => Dynamic a
+  => Active a
 sine
-  = mkDynamic 0 1 $ \t -> sin (2 * pi * realToFrac t)
+  = mkActive 0 1 $ \t -> sin (2 * pi * realToFrac t)
 
 cycleBetween
   :: forall a. Floating a
   => a  -- ^ a1
   -> a  -- ^ seconds between a1 and a2
   -> a  -- ^ a2
-  -> Dynamic a
+  -> Active a
 cycleBetween a1 dt a2
   = (\t -> a1 + (a2-a1) * (1 + t / period) / 2) <$> sine
   where
     period :: a
     period = 2 * dt
 
-dynamic :: Dynamic Picture
-dynamic
+active :: Active Picture
+active
     = thickCircle
   <$> cycleBetween 50 0.5 140
-  <.> cycleBetween 1 0.5 10
+  <*> cycleBetween 1 0.5 10
--- gloss-to-gif/src/GlossToGif.hs
+++ gloss-to-gif/src/GlossToGif.hs
@@ -1,6 +1,6 @@
 module GlossToGif where
 
-import Data.Active (Dynamic)
+import Data.Active (Active, Dynamic)
 import Graphics.Gloss (Picture)
 import Graphics.Gloss.Export.Image (Size)
 import qualified Data.Active as Active
@@ -13,9 +13,33 @@ import qualified Data.Active.Extra as Active
 renderGif
   :: FilePath
   -> Size
+  -> Active Picture
+  -> IO ()
+renderGif filePath size = do
+  Active.onActive (renderImage filePath size)
+                  (renderLoop  filePath size)
+
+renderImage
+  :: FilePath
+  -> Size
+  -> Picture
+  -> IO ()
+renderImage filePath size picture = do
+  Export.exportPicturesToGif
+    0
+    Export.LoopingNever
+    size
+    Gloss.white
+    filePath
+    (const picture)
+    [0]
+
+renderLoop
+  :: FilePath
+  -> Size
   -> Dynamic Picture
   -> IO ()
-renderGif filePath size dynamic = do
+renderLoop filePath size dynamic = do
   Export.exportPicturesToGif
     (ceiling centisecondsPerFrame)  -- at least 1
     Export.LoopingForever
