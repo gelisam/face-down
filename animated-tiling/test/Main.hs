4826d1a3478b0faaa536c3d5a6ef94c948736cab
Samuel Gélineau <gelisam+github@gmail.com>
2019-12-11 06:43:23 -0500

    one repeated, non-animated, non-tiling image

--- animated-tiling/test/Main.hs
+++ animated-tiling/test/Main.hs
@@ -15,19 +15,34 @@ main = defaultMain tests
 
 mkGolden
   :: FilePath
+  -> Tile
   -> TestTree
-mkGolden basename
+mkGolden basename tile
   = goldenVsFile
       basename
       expected
       actual
-      (writeGif actual)
+      (writeGif actual tile)
   where
-    test = $(TH.lift =<< pathRelativeToCabalPackage "test")
     expected = test </> "expected" </> basename <.> "gif"
     actual   = test </> "actual"   </> basename <.> "gif"
+
+test :: FilePath
+test = $(TH.lift =<< pathRelativeToCabalPackage "test")
+
+inputs :: FilePath
+inputs = test </> "inputs"
 
 tests
   :: TestTree
 tests
-  = testGroup "animated-tiling Golden Tests" []
+  = testGroup "animated-tiling Golden Tests"
+  [ mkGolden "9-oranges"
+    $ Tile (0, 0) (48, 48) (inputs </> "orange.png")
+  , mkGolden "16-oranges"
+    $ Tile (24, 24) (48, 48) (inputs </> "orange.png")
+  , mkGolden "packed-oranges"
+    $ Tile (0, 0) (32, 32) (inputs </> "orange.png")
+  , mkGolden "honey"
+    $ Tile (0, 0) (16, 16) (inputs </> "orange.png")
+  ]
